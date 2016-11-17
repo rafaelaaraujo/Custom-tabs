56b84dafd163457d90e2cea9c49a6e007ba35ac8
SUPERATI\rafaela.araujo <rafaela.araujo@supera.com.br>
2016-11-17 15:13:07 -0200

    add volley to gradle

---
+++ app/src/androidTest/java/com/example/rafaelaaraujo/customTabAndroid/MainActivityTest.java
@@ -0,0 +1,49 @@
+package com.example.rafaelaaraujo.customTabAndroid;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import com.example.rafaelaaraujo.customTabAndroid.activity.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.swipeLeft;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isSelected;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.allOf;
+
+/**
+ * Created by rafaela.araujo on 17/11/2016.
+ */
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testSwipeView(){
+        onView(allOf(withId(R.id.viewpager), isDisplayed())).perform(swipeLeft());
+        onView(allOf(withId(R.id.title_tab), isSelected())).check(matches(withText("MAY 15")));
+    }
+
+    @Test
+    public void testClickView(){
+        onView(allOf(withId(R.id.title_tab), withText("APR 15"))).perform(click());
+        onView(allOf(withId(R.id.due_date), isDisplayed())).check(matches(withText("VENCIMENTO EM 20 APR")));
+    }
+
+
+}
