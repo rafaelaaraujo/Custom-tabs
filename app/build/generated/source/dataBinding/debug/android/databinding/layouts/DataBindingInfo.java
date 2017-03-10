6763a228ae99d6c3e451f835ce5e433886262c7a
rafaela.araujo <rafaela.araujo@supera.com.br>
2017-03-10 14:57:28 -0300

    corrigindo alinhamento da string

--- app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -2,5 +2,5 @@ package android.databinding.layouts;
 
 import android.databinding.BindingBuildInfo;
 
-@BindingBuildInfo(buildId="cfc4a46c-1d6a-47dc-907e-b543dcca4a93", modulePackage="com.example.rafaelaaraujo.customTabAndroid", sdkRoot="C:\\Users\\rafaela.araujo\\AppData\\Local\\Android\\Sdk",layoutInfoDir="C:\\Users\\rafaela.araujo\\Documents\\CustomTabAndroid\\app\\build\\intermediates\\data-binding-info\\debug",exportClassListTo="",isLibrary=false,minSdk=18,enableDebugLogs=false,printEncodedError=true)
+@BindingBuildInfo(buildId="673f7cb2-623c-483e-a11e-1fc3e0506faf", modulePackage="com.example.rafaelaaraujo.customTabAndroid", sdkRoot="C:\\Users\\rafaela.araujo\\AppData\\Local\\Android\\Sdk",layoutInfoDir="C:\\Users\\rafaela.araujo\\Documents\\CustomTabAndroid\\app\\build\\intermediates\\data-binding-info\\debug",exportClassListTo="",isLibrary=false,minSdk=18,enableDebugLogs=false,printEncodedError=true)
 public class DataBindingInfo {}
